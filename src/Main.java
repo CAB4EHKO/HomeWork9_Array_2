3c387bee2b4131078c8adb4209ff1590dbe6f15d
Savchenko <savchenko.ra@yandex.ru>
2023-03-01 13:26:33 +0300

    Task №1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        tsak1();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void tsak1() {
+        System.out.println("Task 1.");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element : arr
+             ) {
+            sum += element;
+        }
+        System.out.println(sum);
     }
 }
